3bc9fcfb68decba1cdf5e6cbade0536d0ff8ec08
xander127 <104812985+xander127@users.noreply.github.com>
2026-06-05 17:59:35 -0500

    Primer commit de mi proyecto

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*<body>{children}</body>*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
