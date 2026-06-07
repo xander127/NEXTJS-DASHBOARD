ac98593e12024e90ea156ff5092096609e57f2f0
xander127 <104812985+xander127@users.noreply.github.com>
2026-06-06 19:39:34 -0500

    recuperando cambios

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,8 +10,7 @@ import {
 import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: 'require',
-  prepare: false,
+  ssl: 'require'
 });
 
 export async function fetchRevenue() {
